0b0e139eb3b9e9a21d581c091f19fd946d818e73
Liene Dārta Lukstiņa <lienedarta@gmail.com>
2020-10-21 17:50:28 +0300

    Changed base solution to include premade login

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hermes_chat.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
