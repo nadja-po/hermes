4f8b39615f59cb4c9daf6371bebc87e54462531b
Nadezhda Svetlakova <nadja-po@yandex.ru>
2020-10-25 13:31:30 +0200

    implemented confirmation by email (on the local server) + authentication (not working yet)

--- Startup.cs
+++ Startup.cs
@@ -12,6 +12,8 @@ using Hermes_chat.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.AspNetCore.Identity.UI.Services;
+using Hermes_chat.Services;
 
 namespace Hermes_chat
 {
@@ -33,6 +35,18 @@ namespace Hermes_chat
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
             services.AddControllersWithViews();
+           
+            //facebook authentification
+            services.AddAuthentication().AddFacebook(facebookOptions =>
+            {
+                facebookOptions.AppId = Configuration["Authentication:Facebook:AppId"];
+                facebookOptions.AppSecret = Configuration["Authentication:Facebook:AppSecret"];
+                
+            });
+            //email confirmation
+            services.AddTransient<IEmailSender, EmailSender>();
+            services.Configure<AuthMessageSenderOptions>(Configuration);
+
             services.AddRazorPages();
         }
 
